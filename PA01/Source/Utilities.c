6acb73d40c8014e93fd5ba30feed415086283582
Matthew J. Berger <matthewberger@nevada.unr.edu>
2016-02-01 22:21:06 -0800

    Create Utility function header.

---
+++ PA01/Source/Utilities.c
@@ -0,0 +1,15 @@
+#include "Utilities.h"
+
+void HandleArgs(int argc, char* argv[])
+{
+    /* argc is the number of arguments passed
+       argv contains the arguments
+       argv[0] is the name of the program,
+       so with no arguments, argc is equal to 1. */
+    if( argc < 2 )
+        printf("The input file name is a required argument.\n");
+    else if(argc > 2)
+        printf("Too many arguments received.\n");
+    else
+        printf("The argument supplied is %s\n", argv[1]);
+}
---
+++ PA01/Source/Utilities.h
@@ -0,0 +1,9 @@
+#ifndef _UTILITIES_H
+#define _UTILITIES_H
+
+#include <stdio.h>
+
+// Utility Functions
+void HandleArgs(int argc, char* argv[]);
+
+#endif
