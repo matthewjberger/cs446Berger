1b6d2dc7ab57b5989a66029be3208af0f1a3bf7d
Matthew J. Berger <matthewberger@nevada.unr.edu>
2016-02-01 19:38:59 -0800

    Correct typos in last commit.

--- PA01/Source/Main.c
+++ PA01/Source/Main.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 
 // Function Signatures
-void HandleArgs(argc, char* argv[])
+void HandleArgs(int argc, char* argv[]);
 
-
-    // Main program
+// Main program
 int main(int argc, char *argv[])
 {
     HandleArgs(argc, argv);
-    printf("Hello World\n");
 }
 
 // Function Implementations
-void HandleArgs(argc, char* argv[])
+void HandleArgs(int argc, char* argv[])
 {
     /* argc is the number of arguments passed
        argv contains the arguments
        argv[0] is the name of the program,
        so with no arguments, argc is equal to 1. */
     if( argc < 2 )
-        printf("The input file name is a required argument.");
+        printf("The input file name is a required argument.\n");
     else if(argc > 2)
-        printf("Too many arguments received.");
+        printf("Too many arguments received.\n");
     else
-        printf("The argument supplied is %s\n", argv[0]);
+        printf("The argument supplied is %s\n", argv[1]);
 }
