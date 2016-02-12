0f273f74406bcd4c5a1b11b27d85f51602259932
Matthew J. Berger <matthewberger@nevada.unr.edu>
2016-02-11 18:12:56 -0800

    Read data into structure.

--- PA01/Source/Main.c
+++ PA01/Source/Main.c
@@ -2,108 +2,90 @@
 
 #define MAX_ROWS 30
 #define MAX_COLUMNS 255
+#define MAX_CHILDREN 100
+#define MAX_TIERS 100
 
 #define PrintTabs(numTabs) for(int count = 0; count < numTabs; count++){ printf("    "); }
 
+typedef struct {
+    char *parent_1;
+    char *parent_2;
+    char *children[MAX_CHILDREN];
+    bool hasSpouse;
+} FamilyTier;
+
+const char *delimiters=" \n\t\r\v\f"; // Delimiters for tokenizing by whitespace
+
+bool BuildTier(FamilyTier *tier, char* line);
+
 int main(int argc, char *argv[])
 {
+    // Variables
+    FamilyTier familyTiers[MAX_TIERS];
+    char* filename;
+    FILE* file;
+    char lineBuffer[MAX_COLUMNS];
+    int tierCounter = 0;
+
     // We expect a single argument
     if(!HandleArgs(argc, 1)) return 0;
 
     // Open the file and establish a buffer
-    const char* filename = argv[1];
-    FILE* file = fopen(filename, "r");
-    char lineBuffer[MAX_COLUMNS];
-
-    // Delimiters for tokenizing by whitespace
-    char *delimiters=" \n\t\r\v\f";
-
-    // Number of tabs for formatting
-    int numberOfTabs = 0;
+    filename = argv[1];
+    if(access(filename, F_OK) == -1)
+    {
+        printf("Input file does not exist!\n");
+        return 0;
+    }
+    file = fopen(filename, "r");
 
     // Get the first line of the file
-    if(fgets(lineBuffer, sizeof(lineBuffer), file))
+    while(fgets(lineBuffer, sizeof(lineBuffer), file))
     {
-        // FIRST TIER
-        // Parents
-        char* parent_1 = strtok(lineBuffer, delimiters);
-        if(parent_1 == NULL) return 0;
+        // Ignore blank lines in file
+        if(strcmp(lineBuffer,"\n") == 0) continue;
 
-        char* parent_2 = strtok(NULL, delimiters);
-        if(parent_2 == NULL) return 0;
+        // Fill the struct with data
+        if(!BuildTier(&familyTiers[tierCounter], lineBuffer))
+            printf("Error reading file!\n");
 
-        printf("%s(0)-%s\n", parent_1, parent_2);
-
-        // Fork and print children in new process
-        pid_t pid = fork();
-        numberOfTabs++;
-
-        if (pid != 0)
-        {
-            // SECOND TIER
-            char* childName = strtok(NULL, delimiters);
-            while(childName != NULL)
-            {
-                // Store the current position to return to after parsing the rest of the file
-                fpos_t currentPosition;
-                fgetpos(file, &currentPosition);
-
-                // Check the rest of the file for possible children
-                char childLineBuffer[MAX_COLUMNS];
-                bool foundSpouse = false;
-                while(fgets(childLineBuffer, sizeof(childLineBuffer), file))
-                {
-                    // Ignore blank lines
-                    if(strcmp(childLineBuffer, "") == 0) continue;
-
-                    char* parent_1 = strtok(childLineBuffer, delimiters);
-                    if(parent_1 == NULL) return 0;
-
-                    char* parent_2 = strtok(NULL, delimiters);
-                    if(parent_2 == NULL) return 0;
-
-                    // If the child's name was found in the first or second word
-                    // of any line later in the file, then they have a spouse
-                    if( (strcmp(parent_1, childName) == 0) || (strcmp(parent_2, childName) == 0))
-                    {
-                        foundSpouse = true;
-                        PrintTabs(numberOfTabs);
-                        printf("%s(%i)-%s\n", parent_1, pid, parent_2);
-
-                        // Fork on each child, because they will have children
-                        pid = fork();
-                        numberOfTabs++;
-                        if(pid != 0)
-                        {
-                            PrintTabs(numberOfTabs);
-                            // THIRD TIER
-                            printf("(%i)I'm a third tier process mommy!\n", pid);
-                            return 0;
-                        }
-
-                        break;
-                    }
-                }
-
-                // If spouse was found, simply print the child's name
-                PrintTabs(numberOfTabs);
-                if(!foundSpouse)
-                    printf("%s(%i)\n", childName, pid);
-
-                // Reset the stream indicator
-                fsetpos(file, &currentPosition);
-
-                // Get the next child
-                childName = strtok(NULL, delimiters);
-            }
-        }
-        else
-        {
-            // Wait for child processes to finish
-            waitpid(pid, NULL, 0);
-        }
+        tierCounter++;
     }
 
     // Close the file
     fclose(file);
+}
+
+bool BuildTier(FamilyTier *tier, char* line)
+{
+    // Variables
+    char* parent_1;
+    char* parent_2;
+    int childCounter = 0;
+    char* childName;
+
+    // Parents
+    parent_1 = strtok(line, delimiters);
+    if(parent_1 == NULL) return false;
+    tier->parent_1 = parent_1;
+
+    parent_2 = strtok(NULL, delimiters);
+    if(parent_2 == NULL) return false;
+    tier->parent_2 = parent_2;
+
+printf("%s-%s\n", tier->parent_1, tier->parent_2);
+
+    childName = strtok(NULL, delimiters);
+    while(childName != NULL)
+    {
+        // Add the child name to the current family tier
+        tier->children[childCounter] = childName;
+printf("%s\n", tier->children[childCounter]);
+
+        // Get the next child
+        childName = strtok(NULL, delimiters);
+        childCounter++;
+    }
+
+    return true;
 }
