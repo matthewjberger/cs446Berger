721acc10179ffe6776968fb585ae18cd7a69fa5b
Matthew J. Berger <matthewberger@nevada.unr.edu>
2016-04-22 12:45:01 -0700

    Generalize execution.

---
+++ Sim03/test/Main.cpp
@@ -0,0 +1,38 @@
+#include "Simulator.h"
+using namespace std;
+
+// Utility Functions
+int main( const int argc, const char* argv[] )
+{
+    string configurationFile;
+
+    // Ensure a single argument was provided
+    if( argc != 2 )
+    {
+        cerr << "Error! A configuration file name must be provided." << endl;
+        return EXIT_FAILURE;
+    }
+
+    // Ensure the file exists
+    configurationFile = argv[1];
+    if( access( configurationFile.c_str(), F_OK ) == -1 )
+    {
+        cerr << "Configuration file does not exist!" << endl;
+        return EXIT_FAILURE;
+    }
+
+    try
+    {
+        // Run the simulator
+        Simulator simulator( configurationFile );
+        simulator.run();
+    }
+
+    catch( ... )
+    {
+        cerr << "Aborting program..."<< endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ Sim03/test/Operation.cpp
@@ -0,0 +1,22 @@
+#include "Operation.h"
+
+Operation::Operation( const OperationParameters& parameters )
+{
+    parameters_ = parameters;
+}
+
+bool Operation::completed() const
+{
+    return (parameters_.cyclesLeft == 0);
+}
+
+void Operation::step()
+{
+    parameters_.duration -= parameters_.cycleTime;
+    parameters_.cyclesLeft--;
+}
+
+OperationParameters Operation::parameters() const
+{
+    return parameters_;
+}
